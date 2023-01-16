e9d277c19f5232846994b8e09bec9fe2755dd546
Iceows <mounierr07@gmail.com>
2023-01-16 08:38:48 +0100

    Update 1-GetData.cmd

--- tools/kit/1-GetData.cmd
+++ tools/kit/1-GetData.cmd
@@ -5,7 +5,7 @@ adb push ./arm64/parted /tmp
 adb push ./sh/keepdata.sh /tmp
 
 adb shell "cd /tmp;chmod 777 parted;chmod 777 keepdata.sh"
-adb shell "cd /tmp; bash /tmp/keepdata.sh"
+adb shell "cd /tmp;./keepdata.sh"
 
 adb pull /tmp/HuaweiFileGPT.txt ./phone/HuaweiFileGPT.txt
 adb pull /tmp/HuaweiFilePart.txt ./phone/HuaweiFilePart.txt
