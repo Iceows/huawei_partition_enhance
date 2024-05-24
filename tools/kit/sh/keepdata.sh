b940486ecdc8d79c6a835b843a583cfb37744d03
Iceows <mounierr07@gmail.com>
2024-05-24 18:24:03 +0200

    tools: keepdata remove extra space

--- tools/kit/sh/keepdata.sh
+++ tools/kit/sh/keepdata.sh
@@ -1,14 +1,12 @@
 #!/sbin/sh 
- 
+
  # MMC Disk
 ./parted /dev/block/mmcblk0 --script unit s print quit > /tmp/HuaweiFileGPT-MMC.txt
- 
+
 # UFS disk
 ./parted /dev/block/sdd --script unit s print quit > /tmp/HuaweiFileGPT-UFS.txt
 
-
 ls -la /dev/block/bootdevice/by-name  | grep "\->" > /tmp/HuaweiFilePart.txt
-
 
 chown media_rw:media_rw  /tmp/HuaweiFilePart.txt
 chown media_rw:media_rw /tmp/HuaweiFileGPT-MMC.txt
